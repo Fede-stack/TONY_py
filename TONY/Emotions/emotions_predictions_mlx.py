62d0a2370d6e49b122caf04fda31ec1a52f588e9
Fede-stack <75915084+Fede-stack@users.noreply.github.com>
2026-03-04 09:40:04 +0100

    Create emotions_predictions_mlx.py

---
+++ TONY/Emotions/emotions_predictions_mlx.py
@@ -0,0 +1,28 @@
+from mlx_lm import load, generate
+from mlx_lm.sample_utils import make_sampler
+
+
+class EmotionsPredictor_mlx:
+
+    def __init__(self, model_name: str = "", max_new_tokens: int = 200):
+        self.max_new_tokens = max_new_tokens
+        self.model, self.tokenizer = load(model_name)
+
+    def predict_emotions(self, text: str, max_new_tokens: int = None) -> str:
+        """
+        Predict Emotions for a given text.
+        """
+        prompt = text + " Emotions Output:" 
+
+        sampler = make_sampler(temp=0.0, top_k=10)
+
+        generated_text = generate(
+            self.model,
+            self.tokenizer,
+            prompt=prompt,
+            max_tokens=max_new_tokens or self.max_new_tokens,
+            sampler=sampler,
+            verbose=False,
+        )
+
+        return generated_text.strip()
