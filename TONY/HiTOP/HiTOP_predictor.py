f191b1ebbe39f60af2d2539ae2d101c8170ba9db
Fede-stack <75915084+Fede-stack@users.noreply.github.com>
2026-03-05 19:17:53 +0100

    Update HiTOP_predictor.py

--- TONY/HiTOP/HiTOP_predictor.py
+++ TONY/HiTOP/HiTOP_predictor.py
@@ -4,7 +4,7 @@ import torch
 
 class HiTOP_Predictor:
 
-    def __init__(self, model_name='FritzStack/HiTOP-Llama-3.2-3B_4bit-merged'):
+    def __init__(self, model_name='FritzStack/HiTOP-Llama-3B_4bit'):
 
         self.tokenizer = AutoTokenizer.from_pretrained(
             model_name, trust_remote_code=True
